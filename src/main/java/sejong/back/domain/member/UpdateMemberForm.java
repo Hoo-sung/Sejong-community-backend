309c8a087ba15a558966416c57692a9988e3badd
Hoo-sung <gntjd135@gmail.com>
2023-03-02 22:13:58 +0900

    hoosung3/2-version

---
+++ src/main/java/sejong/back/domain/member/UpdateMemberForm.java
@@ -0,0 +1,15 @@
+package sejong.back.domain.member;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class UpdateMemberForm {
+
+
+    private MemberType type;
+
+//    private List<String> tag;//태그들도 우리가 만들어서 고르는 식으로 해야할거 같음.
+
+}
