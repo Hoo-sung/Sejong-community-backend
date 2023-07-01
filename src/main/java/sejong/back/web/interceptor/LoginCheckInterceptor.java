37bdc16f4dbf276d26b630f2d69e2871bafd972f
Hoo-sung <gntjd135@gmail.com>
2023-07-01 23:22:32 +0900

    requestURI 제대로 수정

--- src/main/java/sejong/back/web/interceptor/LoginCheckInterceptor.java
+++ src/main/java/sejong/back/web/interceptor/LoginCheckInterceptor.java
@@ -19,15 +19,14 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
 
         log.info("인증 체크 인터셉터 실행 {}", requestURI);
         //POST /members 로 요청이 왔을 경우 인터셉터를 성공시킨다.
-        if (requestURI.equals("/members") && request.getMethod().equals("POST")) return true;
+//        if (requestURI.equals("/members") && request.getMethod().equals("POST")) return true;
 
         HttpSession session = request.getSession(false);//세션을 있는 그대로 가져온다.
 
-        /**
-         * 예외적으로 /members post인 경우에는 인터셉터가 적용 안되게 하자.
-         */
 
-        if(request.getMethod().equals("POST") &&request.getRequestURI().equals("/members")){
+        if(request.getMethod().equals("POST") &&request.getRequestURI().equals("http://ec2-3-24-166-96.ap-southeast-2.compute.amazonaws.com:8080/members")){
+            log.info("/members POST는 필터를 통과시킨다.");
+
             return true;//controller로 쭉 진행.
         }
         else if (session == null || session.getAttribute(SessionConst.DB_KEY) == null) {//로그인 안된거면 로그인페이지로 리다이렉트
